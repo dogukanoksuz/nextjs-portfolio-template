fe9e1abf3b64b689dd5e6de337cecfd1ac9d94f0
dogukanoksuz <me@dogukan.dev>
2024-05-16 14:08:41 +0300

    feature: Add Next.js deployment workflow and update next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+    basePath: "/nextjs-portfolio-template",
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
